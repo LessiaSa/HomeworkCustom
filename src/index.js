cf15622e139157f0c1a5e8b5516aed24761b9b00
LessiaSa <0162221@mail.ru>
2025-01-15 10:14:42 +0700

    CorrectedTheJsonCode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-debugger */
 const func = () => {
-    const sum = (a,b) => a + b;
-    let x = 10;;
-    let y = 20
+    const sum = (a, b) => a + b;
+    let x = 10;
+    let y = 20;
 
     console.log(sum(x, y)); // 30
     debugger;
-    x = 20
-    y = 30
+    x = 20;
+    y = 30;
 
     console.log(sum(x, y)); // 50
 };
